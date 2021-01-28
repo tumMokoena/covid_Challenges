ab01979d8b5b2752ff6de62cb8fb12b2418d4be6
tumMokoena <61909721+tumMokoena@users.noreply.github.com>
2021-01-28 08:41:17 +0200

    second commit

--- script.js
+++ script.js
@@ -5,8 +5,42 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
+//functions for handling inputs
+function showError(input, message){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+function showSuccess (input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+//check email is valid
+function isValidEmail(email){
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+// check if input are empty
+
+function checkInput(inputArr){
+    inputArr.forEach(input => {
+
+        if(input.value.trim() === ''){
+            showError(input, `${input.id} is required`);
+        } else {
+            showSuccess(input);
+        }
+    });
+}
+
 // add event listener for button submit
 form.addEventListener('submit', function(e) {
-    e
-    console.log(username.value);
+    e.preventDefault();
+    
+    checkInput([username,email,password,password2]);
 })
